1f76a38aa77c5d9cd42545682550be9a868ea490
jmcgi82 <152920287+jmcgi82@users.noreply.github.com>
2024-06-12 13:04:09 -0400

    Added Multiple series in linechart

--- src/main/java/usr/mcgill/datavisualization/Linechart/Linechart.java
+++ src/main/java/usr/mcgill/datavisualization/Linechart/Linechart.java
@@ -1,6 +1,7 @@
 package usr.mcgill.datavisualization.Linechart;
 
 import javafx.application.Application;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -38,9 +39,40 @@ public class Linechart extends Application {
         series.getData().add(new XYChart.Data(11, 29));
         series.getData().add(new XYChart.Data(12, 25));
 
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Dad's Portfolio");
+
+        series2.getData().add(new XYChart.Data(1, 30));
+        series2.getData().add(new XYChart.Data(2, 23));
+        series2.getData().add(new XYChart.Data(3, 35));
+        series2.getData().add(new XYChart.Data(4, 24));
+        series2.getData().add(new XYChart.Data(5, 34));
+        series2.getData().add(new XYChart.Data(6, 36));
+        series2.getData().add(new XYChart.Data(7, 42));
+        series2.getData().add(new XYChart.Data(8, 45));
+        series2.getData().add(new XYChart.Data(9, 43));
+        series2.getData().add(new XYChart.Data(10, 55));
+        series2.getData().add(new XYChart.Data(11, 49));
+        series2.getData().add(new XYChart.Data(12, 48));
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("Dog's Portfolio");
+        //populating the series with data
+        series3.getData().add(new XYChart.Data(1, 15));
+        series3.getData().add(new XYChart.Data(2, 14));
+        series3.getData().add(new XYChart.Data(3, 15));
+        series3.getData().add(new XYChart.Data(4, 10));
+        series3.getData().add(new XYChart.Data(5, 6));
+        series3.getData().add(new XYChart.Data(6, 20));
+        series3.getData().add(new XYChart.Data(7, 22));
+        series3.getData().add(new XYChart.Data(8, 60));
+        series3.getData().add(new XYChart.Data(9, 11));
+        series3.getData().add(new XYChart.Data(10, 17));
+        series3.getData().add(new XYChart.Data(11, 15));
+        series3.getData().add(new XYChart.Data(12, 12));
 
         Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().add(series);
+        lineChart.getData().addAll(series, series2, series3);
 
         stage.setScene(scene);
         stage.show();
